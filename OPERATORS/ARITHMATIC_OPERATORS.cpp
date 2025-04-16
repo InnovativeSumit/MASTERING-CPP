e65cc60ddf1560620cb210941f232728df1f6f88
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-16 13:10:11 +0530

    ARITHMATIC-OPERATORS
    Arithmetic operators perform basic math operations:
    + (Addition)
    - (Subtraction)
    * (Multiplication)
    / (Division)
    % (Modulus: remainder)

---
+++ OPERATORS/ARITHMATIC_OPERATORS.cpp
@@ -0,0 +1,27 @@
+//make by sumit
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,e,f,g;
+a=20;
+b=2;
+cout<<"THE ADDITION IS       :"<<(a+b)<<endl;
+cout<<"THE SUBTRACTION IS    :"<<(a-b)<<endl;
+cout<<"THE MULTIPLICATION IS :"<<(a*b)<<endl;
+cout<<"THE DIVISION IS       :"<<(a/b)<<endl;
+cout<<"THE REMIANDER IS      :"<<(a%b)<<endl<<"\n\n"; //%-> MODULO OPERATORS
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+g=a%b;
+cout<<"NOW WE STORE THE OPERSTION IN OTHER VARIABLE"<<endl;
+cout<<"============================================"<<endl;
+cout<<"THE ADDITION IS       :"<<c<<endl;
+cout<<"THE SUBTRACTION IS    :"<<d<<endl;
+cout<<"THE MULTIPLICATION IS :"<<e<<endl;
+cout<<"THE DIVISION IS       :"<<f<<endl;
+cout<<"THE REMIANDER IS      :"<<g<<endl;
+return 0;
+}
+
