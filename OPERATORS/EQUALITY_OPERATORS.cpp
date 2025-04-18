ba24452f66e99cdf607f058a5f5929db7d0256a6
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-18 10:36:51 +0530

    EQUALITY-OPERATORS
    Equal to (==)
    Checks if two values are the same.
    Example: 5 == 5 → True
    
    Not equal to (!= or <>)
    Checks if two values are different.
    Example: 5 != 3 → True

---
+++ OPERATORS/EQUALITY_OPERATORS.cpp
@@ -0,0 +1,13 @@
+// MAKE BY SUMIT
+#include<iostream>
+using namespace std;
+int main() {
+int a = 3;
+int b = 4;
+// == represents equals to
+cout<<(a == b)<<endl; // Output: 0 (false)
+// != represents not equals to
+cout<<(a != b);// Output: 1 (true)
+return 0;
+}
+
