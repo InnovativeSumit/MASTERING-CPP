caa3f0c13baa75b9c145cdf67c2dee594eb44d5c
InnovativeSumit <pal2024santosh@gmail.com>
2025-04-27 09:03:08 +0530

    TERNARY-OPERATORS
    The ternary operator is a shorthand way of writing an if-else statement.
    It’s called "ternary" because it involves three operands.
    
    condition → an expression that is either true or false
    expression_if_true → runs if the condition is true
    expression_if_false → runs if the condition is false

---
+++ OPERATORS/TERNARY-OPERATOR.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10, b = 20,max;
+
+// Using ternary operator to find the maximum
+max = (a > b) ? a : b;
+cout << "The maximum value is: " << max << endl;
+return 0;
+}
